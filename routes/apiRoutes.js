e4b33f7017900fe741e73110d3e12c7f03999b32
rybo1217 <brownryan1217@yahoo.com>
2022-10-31 23:52:56 -0400

    yes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@ import db from '../database/initializeDB.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the UMD Dining API!');
+  res.send('Welcome to the API!');
 });
 
 router.route("/disasters")
